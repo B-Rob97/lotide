2bd3fbde3b757645ef9d4d59a6194240947e732f
Brett Roberts <brett.roberts@live.ca>
2023-08-22 17:21:22 -0600

    Added one more test case

--- eqArrays.js
+++ eqArrays.js
@@ -18,7 +18,8 @@ const eqArrays = function(array1, array2) {
   return true;
 };
 
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); //=> true
+assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);  //=> false
 assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true); // => true
 assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);  //=> false
+assertEqual(eqArrays([], []), true); // => true
