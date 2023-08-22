66543cafd7d2df458cf36c92f6cf49d579e06c88
Brett Roberts <brett.roberts@live.ca>
2023-08-22 15:26:54 -0600

    Created eqArrays file and removed emojis from assertEquals.js

--- eqArrays.js
+++ eqArrays.js
@@ -5,10 +5,3 @@ const assertEqual = function(actual, expected) {
     console.log(`✅Assertion Passed: ${actual} === ${expected}✅`);
   }
 };
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
-assertEqual(5, 'Five');
-assertEqual(100, 100);
-assertEqual("Brett", "Brett");
-assertEqual("Brett", "Bret");
-assertEqual(5, 5);
