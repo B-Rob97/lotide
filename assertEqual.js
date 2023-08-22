1a67f193d79d2af250ec601e4daeb14c692abab9
Brett Roberts <brett.roberts@live.ca>
2023-08-21 18:17:44 -0600

    Added function definition for assertEqual and various function executions to test.

---
+++ assertEqual.js
@@ -0,0 +1,14 @@
+const assertEqual = function(actual, expected) {
+  if (actual !== expected) {
+    console.log("❌⛔❌Assertion Failed: " + actual + " !== " + expected + "❌⛔❌");
+  } else if (actual === expected) {
+    console.log("✅😁✅Assertion Passed: " + actual + " === " + expected + "✅😁✅");
+  }
+};
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual(5, 'Five');
+assertEqual(100, 100);
+assertEqual("Brett", "Brett");
+assertEqual("Brett", "Bret");
+assertEqual(5, 5);
