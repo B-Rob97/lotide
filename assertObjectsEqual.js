5397f480e4ca1d6b0d04a6002fa9e1fe13faf90d
Brett Roberts <brett.roberts@live.ca>
2023-08-23 20:27:36 -0600

    Initial commit

---
+++ assertObjectsEqual.js
@@ -0,0 +1,18 @@
+const eqObjects = function(object1, object2) {
+  const keys1 = Object.keys(object1);
+  const keys2 = Object.keys(object2);
+  if (keys1.length !== keys2.length) {
+    return false;
+  }
+  for (const key of keys1) {
+    if (Array.isArray(object1[key]) || Array.isArray(object1[key])) {
+      eqArrays(object1[key], object2[key])
+    } else if (object1[key] !== object2[key]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const assertObjectsEqual = function(actual, expected) {
+};
