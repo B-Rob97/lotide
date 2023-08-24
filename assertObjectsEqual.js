ac747fa103a54960bfd06eb2dc395f0c22c6d7fa
Brett Roberts <brett.roberts@live.ca>
2023-08-23 21:18:57 -0600

    Fixed logic error in eqObjects that was causing assertObjectsEqual to fail

--- assertObjectsEqual.js
+++ assertObjectsEqual.js
@@ -1,18 +1,51 @@
-const eqObjects = function(object1, object2) {
-  const keys1 = Object.keys(object1);
-  const keys2 = Object.keys(object2);
-  if (keys1.length !== keys2.length) {
+const eqArrays = function(array1, array2) {
+  if (array1.length !== array2.length) {
     return false;
   }
-  for (const key of keys1) {
-    if (Array.isArray(object1[key]) || Array.isArray(object1[key])) {
-      eqArrays(object1[key], object2[key])
-    } else if (object1[key] !== object2[key]) {
+  for (let x = 0; x < array1.length; x++) {
+    if (array1[x] !== array2[x]) {
       return false;
     }
   }
   return true;
 };
 
-const assertObjectsEqual = function(actual, expected) {
+const eqObjects = function(object1, object2) {
+  const keys1 = Object.keys(object1);
+  const keys2 = Object.keys(object2);
+
+  if (keys1.length !== keys2.length) {
+    return false;
+  }
+
+  for (const key of keys1) {
+    if (Array.isArray(object1[key]) && Array.isArray(object2[key])) {
+      if (!eqArrays(object1[key], object2[key])) {
+        return false;
+      }
+    } else if (object1[key] !== object2[key]) {
+      return false;
+    }
+  }
+
+  return true;
 };
+
+
+const assertObjectsEqual = function(actual, expected) {
+  const inspect = require('util').inspect;
+  if (eqObjects(actual, expected)) {
+    console.log(`✅Assertion Passed: ${inspect(actual)} === ${inspect(expected)}✅`);
+  } else {
+    console.log(`❌Assertion Failed: ${inspect(actual)} !== ${inspect(expected)}❌`);
+  }
+};
+
+
+const obj1 = { a: 1, b: 2 };
+const obj2 = { b: 2, a: 1 };
+assertObjectsEqual(obj1, obj2);
+
+const obj3 = { a: 1, b: { c: 2 } };
+const obj4 = { a: 1, b: { c: 2 } };
+assertObjectsEqual(obj3, obj4);
--- eqObjects.js
+++ eqObjects.js
@@ -21,18 +21,25 @@ const assertEqual = function(actual, expected) {
 const eqObjects = function(object1, object2) {
   const keys1 = Object.keys(object1);
   const keys2 = Object.keys(object2);
+
   if (keys1.length !== keys2.length) {
     return false;
   }
+
   for (const key of keys1) {
-    if (Array.isArray(object1[key]) || Array.isArray(object1[key])) {
-      eqArrays(object1[key], object2[key])
+    if (Array.isArray(object1[key]) && Array.isArray(object2[key])) {
+      if (!eqArrays(object1[key], object2[key])) {
+        return false;
+      }
     } else if (object1[key] !== object2[key]) {
       return false;
     }
   }
+
   return true;
 };
+
+
   
 const shirtObject = { color: "red", size: "medium" };
 const anotherShirtObject = { size: "medium", color: "red" };
@@ -49,24 +56,6 @@ const anotherMultiColorShirtObject = { size: "medium", colors: ["red", "blue"] }
 eqObjects(multiColorShirtObject  , anotherMultiColorShirtObject); // => true
 assertEqual(eqObjects(multiColorShirtObject  , anotherMultiColorShirtObject), true);
 
-const longSleeveMultiColorShirtObject= { size: "medium", colors: ["red", "blue"], sleeveLength: "long" };
+const longSleeveMultiColorShirtObject = { size: "medium", colors: ["red", "blue"], sleeveLength: "long" };
 eqObjects(multiColorShirtObject  , longSleeveMultiColorShirtObject); // => false
-assertEqual(eqObjects(multiColorShirtObject  , longSleeveMultiColorShirtObject), false)
-
-// GPT Tests cause my brain hurts
-const differentValueSameKey = { color: "blue", size: "medium" };
-assertEqual(eqObjects(shirtObject, differentValueSameKey), false);
-
-const extraKeyObject = { color: "red", size: "medium", material: "cotton" };
-assertEqual(eqObjects(shirtObject, extraKeyObject), false);
-
-const emptyObjects = {};
-assertEqual(eqObjects({}, {}), true);
-
-const differentKeysObject = { style: "polo", size: "medium", color: "red" };
-assertEqual(eqObjects(shirtObject, differentKeysObject), false);
-
-const nestedObject1 = { color: "red", size: "medium", details: { material: "cotton" } };
-
-const nestedObjectMismatch = { color: "red", size: "medium", details: { material: "polyester" } };
-assertEqual(eqObjects(nestedObject1, nestedObjectMismatch), false); 
+assertEqual(eqObjects(multiColorShirtObject  , longSleeveMultiColorShirtObject), false);
