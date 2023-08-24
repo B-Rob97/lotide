f338757c10cd4f50bdf8779f099ac2ad171aa11e
Brett Roberts <brett.roberts@live.ca>
2023-08-24 07:39:05 -0600

    wrote better assertion tests

--- assertObjectsEqual.js
+++ assertObjectsEqual.js
@@ -42,10 +42,19 @@ const assertObjectsEqual = function(actual, expected) {
 };
 
 
+// Test cases for assertObjectsEqual
 const obj1 = { a: 1, b: 2 };
 const obj2 = { b: 2, a: 1 };
-assertObjectsEqual(obj1, obj2);
+assertObjectsEqual(obj1, obj2); // Should pass
 
-const obj3 = { a: 1, b: { c: 2 } };
-const obj4 = { a: 1, b: { c: 2 } };
-assertObjectsEqual(obj3, obj4);
+const obj3 = { a: 1, b: [2, 3] };
+const obj4 = { a: 1, b: [2, 3] };
+assertObjectsEqual(obj3, obj4); // Should pass
+
+const obj5 = { x: 1, y: [2, 3] };
+const obj6 = { x: 1, y: [3, 2] };
+assertObjectsEqual(obj5, obj6); // Should fail
+
+const obj7 = { p: 1, q: 2 };
+const obj8 = { q: 2, p: 1, r: 3 };
+assertObjectsEqual(obj7, obj8); // Should fail
