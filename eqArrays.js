5325ff3207d7611249f6ff143893ce20a580657c
Brett Roberts <brett.roberts@live.ca>
2023-08-28 08:54:29 -0600

    refactored eqArrays

--- eqArrays.js
+++ eqArrays.js
@@ -1,11 +1,3 @@
-const assertEqual = function(actual, expected) {
-  if (actual !== expected) {
-    console.log(`❌Assertion Failed: ${actual} !== ${expected}❌`);
-  } else if (actual === expected) {
-    console.log(`✅Assertion Passed: ${actual} === ${expected}✅`);
-  }
-};
-
 const eqArrays = function(array1, array2) {
   if (array1.length !== array2.length) {
     return false;
@@ -18,8 +10,4 @@ const eqArrays = function(array1, array2) {
   return true;
 };
 
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); //=> true
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);  //=> false
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true); // => true
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);  //=> false
-assertEqual(eqArrays([], []), true); // => true
+module.exports = eqArrays;
