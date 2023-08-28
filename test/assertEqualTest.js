85cd72247dc35f991d829e76bfd85d00c8577c99
Brett Roberts <brett.roberts@live.ca>
2023-08-28 08:08:23 -0600

    Refactored assertEqual function definition. Implemented export to dry up assertEqual & head. Created a seperate test directory with test files for assertEqual.js and head.js to further dry up code

---
+++ test/assertEqualTest.js
@@ -0,0 +1,9 @@
+const assertEqual = require('../assertEqual');
+
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual(5, 'Five');
+assertEqual(100, 100);
+assertEqual("Brett", "Brett");
+assertEqual("Brett", "Bret");
+assertEqual(5, '5');
---
+++ test/headTest.js
@@ -0,0 +1,8 @@
+const head = require('../head.js');
+const assertEqual = require('../assertEqual.js')
+
+assertEqual(head([5,6,7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+console.log(head([1, 2, 3]));
+console.log(head([1]));
+console.log(head([]));
