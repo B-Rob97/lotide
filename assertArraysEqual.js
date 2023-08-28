e5c7e9c06dfe8f5bab236e2a9d44836e3243953c
Brett Roberts <brett.roberts@live.ca>
2023-08-28 09:27:41 -0600

    Refactored assertArraysEqual function definition. Created assertArraysEqualTest file. Incorporated module export into assertArraysEqual to dry up code

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,25 +1,16 @@
-const eqArrays = function(array1, array2) {
-  if (array1.length !== array2.length) {
-    return false;
-  }
-  for (let x = 0; x < array1.length; x++) {
-    if (array1[x] !== array2[x]) {
-      return false;
-    }
-  }
-  return true;
-};
+const eqArrays = require("./eqArrays");
 
 const assertArraysEqual = function(array1, array2) {
+  
   if (eqArrays(array1, array2)) {
     console.log(`✅Arrays Equal: ${array1} === ${array2}✅`);
-  } else {
-    console.log(`❌Arrays NOT Equal: ${array1} === ${array2}❌`);
   }
+  
+  if (!eqArrays(array1, array2)) {
+    console.log(`❌Arrays NOT Equal: ${array1} !== ${array2}❌`);
+  }
+  
 };
 
-assertArraysEqual([1, 2, 3], [1, 2, 3]); //=> true
-assertArraysEqual([1, 2, 3], [3, 2, 1]);  //=> false
-assertArraysEqual(["1", "2", "3"], ["1", "2", "3"]); // => true
-assertArraysEqual(["1", "2", "3"], ["1", "2", 3]);  //=> false
-assertArraysEqual([], []); // => true
+module.exports = assertArraysEqual;
+
