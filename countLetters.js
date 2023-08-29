4d307e9632c1f575a9f019f5b1bd4f3b2b647959
Brett Roberts <brett.roberts@live.ca>
2023-08-28 20:31:02 -0600

    refactored to remove else statements to improve readability

--- countLetters.js
+++ countLetters.js
@@ -1,14 +1,13 @@
 const countLetters = function(letters) {
   let result = {};
   for (let letter of letters) {
-    if (letter !== " ") {
-      letter = letter.toLowerCase();
-      if (result[letter]) {
-        result[letter] += 1;
-      } else {
-        result[letter] = 1;
-      }
+    if (letter === " ") {
+      continue;
     }
+    if (!result[letter]) {
+      result[letter] = 0;
+    }
+    result[letter]++;
   }
   return result;
 };
